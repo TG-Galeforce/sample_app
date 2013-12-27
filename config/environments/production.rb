0a1d8682c06480d6d89a3c4fe9e43a238583c9f1
Joseph L <josephtwli@berkeley.edu>
2013-12-27 15:05:16 -0800

    Minor debugging for Heroku/production version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #Fallback to assets pipeline
+  config.assets.compile = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
